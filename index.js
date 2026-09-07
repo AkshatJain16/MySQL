52cb67a5bee37716783864f5010a888768c3006b
AkshatJain16 <akshatjain0223@gmail.com>
2026-09-07 20:50:24 +0530

    Bus booking system

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root', 
-  password: '',//enter your password here
+  password: 'Akj@2302',//enter your password here
   database: 'testdb'
 });
 
@@ -16,17 +16,56 @@ connection.connect((err) => {
   }
   console.log('Connection has been created');
 
-  const createTableQuery = `create table Students(
+  const createUserTable = `create table Users(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     email VARCHAR(255)
   )`
-  connection.execute(createTableQuery, (err) => {
+  connection.execute(createUserTable, (err) => {
     if (err) {
       console.error(err);
       return;
     }
-    console.log('Table is created');
+    console.log(' User table is created');
+  });
+
+  const createBusesTable = `create table Buses(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    busNumber VARCHAR(255),
+    totalSeats INT,
+    availableSeats INT
+  )`
+  connection.execute(createBusesTable, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(' Bus table is created');
+  });
+
+  const createBookingTable = `create table Bookings(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    seatNumber INT
+  )`
+  connection.execute(createBookingTable, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(' Booking table is created');
+  });
+
+  const createPaymentsTable = `create table Payments(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    amountPaid DECIMAL(10, 2),
+    paymentStatus VARCHAR(255)
+  )`
+  connection.execute(createPaymentsTable, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(' Payments table is created');
   });
 });
 
