f5cc18cadcd76f37ee0645aaea0ef4c2ce0bd63d
AkshatJain16 <akshatjain0223@gmail.com>
2026-09-08 19:27:26 +0530

    Add, update, delete entries

--- utils/db-connection.js
+++ utils/db-connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root', 
-  password: 'Akj@2302',//enter your password here
+  password: '',//enter your password here
   database: 'testdb'
 });
 
