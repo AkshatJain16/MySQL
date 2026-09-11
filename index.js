c2739915491674dd0f5694715cd529376096cf8e
AkshatJain16 <akshatjain0223@gmail.com>
2026-09-11 20:46:11 +0530

    Added sequilize

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const db = require('./utils/db-connection');
 const studentsRoutes = require('./routes/studentRoutes');
+
+
+const studentModel = require('./models/students');
 // const busRoutes  = require('./routes/busRoutes');
 // const userRoutes = require('./routes/userRoutes');
 const app = express();
@@ -10,9 +14,18 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.use("/students",studentsRoutes);
+// app.use("/students",studentsRoutes);
 // app.use("/buses",busRoutes);
 // app.use("/users", userRoutes);
+
+db.sync({force:true}).then(()=>{
+  app.listen(3000, (err)=>{
+    console.log("Server is running");
+  })
+})
+.catch((err)=>{
+  console.log(err);
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- utils/db-connection.js
+++ utils/db-connection.js
@@ -1,84 +1,102 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root', 
+//   password: 'Akj@2302',//enter your password here
+//   database: 'testdb'
+// });
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log('Connection has been created');
+
+//   const createStudentTable = `create table IF NOT EXISTS Students(
+//     id INT AUTO_INCREMENT PRIMARY KEY,
+//     name VARCHAR(255),
+//     email VARCHAR(255) UNIQUE,
+//     age INT
+//   )`;
+//   connection.execute(createStudentTable, (err) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     console.log('Student table is created');
+//   });
+
+//   const createUserTable = `create table IF NOT EXISTS Users(
+//     id INT AUTO_INCREMENT PRIMARY KEY,
+//     name VARCHAR(255),
+//     email VARCHAR(255)
+//   )`
+//   connection.execute(createUserTable, (err) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     console.log('User table is created');
+//   });
+
+//   const createBusTable = `create table IF NOT EXISTS Buses(
+//     id INT AUTO_INCREMENT PRIMARY KEY,
+//     busNumber VARCHAR(255),
+//     totalSeats INT,
+//     availableSeats INT
+//   )`
+//   connection.execute(createBusTable, (err) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     console.log('Buses table is created');
+//   });
+
+//   const createBookingTable = `create table IF NOT EXISTS Bookings(
+//     id INT AUTO_INCREMENT PRIMARY KEY,
+//     seatNumber INT
+//   )`
+//   connection.execute(createBookingTable, (err) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     console.log('Bookings table is created');
+//   });
+
+//   const createPaymentTable = `create table IF NOT EXISTS Payments(
+//     id INT AUTO_INCREMENT PRIMARY KEY,
+//     amountPaid DECIMAL(10, 2),
+//     paymentStatus VARCHAR(255)
+//   )`
+//   connection.execute(createPaymentTable, (err) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     console.log('Payment table is created');
+//   });
+// });
+
+// module.exports = connection;
+
+const {Sequelize} =  require('sequelize');
+
+const sequelize = new Sequelize('testdb','root','Akj@2302',{
   host: 'localhost',
-  user: 'root', 
-  password: '',//enter your password here
-  database: 'testdb'
+  dialect: 'mysql'
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log('Connection has been created');
+(async() => {try {
+  await sequelize.authenticate();
+  console.log("Connection to the database has been created");
 
-  const createStudentTable = `create table IF NOT EXISTS Students(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255) UNIQUE,
-    age INT
-  )`;
-  connection.execute(createStudentTable, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log('Student table is created');
-  });
+} catch (error) {
+  
+  console.log(error);
+}})();
 
-  const createUserTable = `create table IF NOT EXISTS Users(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255)
-  )`
-  connection.execute(createUserTable, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log('User table is created');
-  });
-
-  const createBusTable = `create table IF NOT EXISTS Buses(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    busNumber VARCHAR(255),
-    totalSeats INT,
-    availableSeats INT
-  )`
-  connection.execute(createBusTable, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log('Buses table is created');
-  });
-
-  const createBookingTable = `create table IF NOT EXISTS Bookings(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    seatNumber INT
-  )`
-  connection.execute(createBookingTable, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log('Bookings table is created');
-  });
-
-  const createPaymentTable = `create table IF NOT EXISTS Payments(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    amountPaid DECIMAL(10, 2),
-    paymentStatus VARCHAR(255)
-  )`
-  connection.execute(createPaymentTable, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log('Payment table is created');
-  });
-});
-
-module.exports = connection;
+module.exports = sequelize;
